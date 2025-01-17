0ada5e97e0c9e3c8b90cda6f54ff075bacef3ef7
jshonfei <69642083+jshonfei@users.noreply.github.com>
2025-01-17 16:50:21 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["aidesk.tkdncloud.net"];
-pub const PUBLIC_RS_PUB_KEY: &str = "5B1FyRxWMVaaA1kOk1ExAOv1Zhll2q0zyQXZCvXbpmo=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["111"];
+pub const PUBLIC_RS_PUB_KEY: &str = "1111";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
